7d179cdabc69f7c92b4334162687fb7400f4ca28
AthinaVek <athina.vekraki@gmail.com>
2021-01-05 17:09:59 +0200

    Read classes file

--- cluster.cpp
+++ cluster.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 
 int main(int argc, char** argv){
-	string iFile, iFile2, classes, confFile, oFile;
+	string iFile, iFile2, classes, confFile, oFile, sline;
 	int magic_number=0, number_of_images=0;
     int n_rows=0, n_cols=0, n_rows2=0, n_cols2=0, d, d2, count=0;
-    int k, L, kl; 
+    int k, L, kl, countl=0, countw=0; 
     int changes = 6, first=1;
     unsigned int dist;
     bool exists;
+    char *cline, *pch;
 
     vector< vector<unsigned short> > pVec2, centroids2;
     vector<unsigned short> tempVec2, tempC2, pDim2;
     
 	vector< vector<unsigned char> > pVec, centroids;
 	vector<unsigned char> tempVec, pDim, tempC;
-	vector< vector<int> > clusters, clusters2, temp;
-	vector< vector<int> > sVec;
+	vector< vector<int> > clusters, clusters2, clusters3, temp, sVec;
 	vector<int> aVec, tempIntVec, pos;
 	vector< vector<distanceNode> > distRange;
 	vector<distanceNode> distTemp;
@@ -34,129 +34,156 @@ int main(int argc, char** argv){
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-	ifstream file2 (iFile2);
-	ofstream ofile (oFile);
-	if (file2.is_open()){
-		read_data2(file2, &magic_number, &number_of_images, &n_rows2, &n_cols2, pVec2, tempVec2);
+// 	ifstream file2 (iFile2);
+// 	ofstream ofile (oFile);
+// 	if (file2.is_open()){
+// 		read_data2(file2, &magic_number, &number_of_images, &n_rows2, &n_cols2, pVec2, tempVec2);
 
-		for(int i = 0; i < k; i++){
-			clusters2.push_back(vector<int>());
-			temp.push_back(vector<int>());
-		} 
-		d2 = n_rows2 * n_cols2;
+// 		for(int i = 0; i < k; i++){
+// 			clusters2.push_back(vector<int>());
+// 			temp.push_back(vector<int>());
+// 		} 
+// 		d2 = n_rows2 * n_cols2;
 		
-		auto t1 = chrono::high_resolution_clock::now();
+// 		auto t1 = chrono::high_resolution_clock::now();
 		
-		k_means_init2(centroids2, number_of_images, pVec2, k, d2);
+// 		k_means_init2(centroids2, number_of_images, pVec2, k, d2);
 		
-		if (ofile.is_open()){															//Lloyd's
-			ofile << "NEW SPACE" << endl;
+// 		if (ofile.is_open()){															//Lloyd's
+// 			ofile << "NEW SPACE" << endl;
 
-			while((count < 40) && (changes > 5)){
-				changes = 0;
-				lloyds_assignment2(clusters2, temp, number_of_images, pVec2, centroids2, k, d2, &changes, first);
+// 			while((count < 40) && (changes > 5)){
+// 				changes = 0;
+// 				lloyds_assignment2(clusters2, temp, number_of_images, pVec2, centroids2, k, d2, &changes, first);
 				
-				if(!first){
-					if (changes <= 5)
-						break;
-				}
-				else{
-					changes = 6;
-				}	
+// 				if(!first){
+// 					if (changes <= 5)
+// 						break;
+// 				}
+// 				else{
+// 					changes = 6;
+// 				}	
 				
-				centroids2.erase(centroids2.begin(), centroids2.end());                      
-				update_centroids_median2(centroids2, pDim2, pVec2, clusters2, tempC2, k, d2);    		// new centroids
+// 				centroids2.erase(centroids2.begin(), centroids2.end());                      
+// 				update_centroids_median2(centroids2, pDim2, pVec2, clusters2, tempC2, k, d2);    		// new centroids
 				
-				first = 0;
-				count++;
-			}
-			auto t2 = chrono::high_resolution_clock::now();
-			auto durationLloyds2 = chrono::duration_cast<chrono::microseconds>( t2 - t1 ).count();
+// 				first = 0;
+// 				count++;
+// 			}
+// 			auto t2 = chrono::high_resolution_clock::now();
+// 			auto durationLloyds2 = chrono::duration_cast<chrono::microseconds>( t2 - t1 ).count();
 
-			for(int i=0; i<k; i++){
-				ofile << "CLUSTER-" << i << " {size: " << clusters2[i].size() << ", centroid: [";
-				for (int y=0; y<d2-1; y++){
-					ofile << (int)centroids2[i][y] << ", ";
-				}
-				ofile << (int)centroids2[i][d2-1] << "]}" << endl;
-			}
+// 			for(int i=0; i<k; i++){
+// 				ofile << "CLUSTER-" << i << " {size: " << clusters2[i].size() << ", centroid: [";
+// 				for (int y=0; y<d2-1; y++){
+// 					ofile << (int)centroids2[i][y] << ", ";
+// 				}
+// 				ofile << (int)centroids2[i][d2-1] << "]}" << endl;
+// 			}
 
-			ofile << "clustering_time: " << durationLloyds2 << endl;
-			silhouette2(clusters2, centroids2, pVec2, k, d2, ofile);
-		}
-		else{
-			cout << "Output file does not exist." << endl;
-		}
-	}
+// 			ofile << "clustering_time: " << durationLloyds2 << endl;
+// 			silhouette2(clusters2, centroids2, pVec2, k, d2, ofile);
+// 		}
+// 		else{
+// 			cout << "Output file does not exist." << endl;
+// 		}
+// 	}
 
-	temp.erase(temp.begin(), temp.end());
-	first = 1;
-	changes = 6;
-
-
+// 	temp.erase(temp.begin(), temp.end());
+// 	first = 1;
+// 	changes = 6;
 
 
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// 	ifstream file (iFile);
+// 	if (file.is_open()){
+// 		read_data(file, &magic_number, &number_of_images, &n_rows, &n_cols, pVec, tempVec);
 
+// 		for(int i = 0; i < k; i++){
+// 			clusters.push_back(vector<int>());
+// 			temp.push_back(vector<int>());
+// 		} 
+// 		d = n_rows * n_cols;
+		
+// 		auto t3 = chrono::high_resolution_clock::now();
+		
+// 		k_means_init(centroids, number_of_images, pVec, k, d);
+		
+// 		if (ofile.is_open()){															//Lloyd's
+// 			ofile << endl << "ORIGINAL SPACE" << endl;
+
+// 			while((count < 40) && (changes > 5)){
+// 				changes = 0;
+// 				lloyds_assignment(clusters, temp, number_of_images, pVec, centroids, k, d, &changes, first);
+				
+// 				if(!first){
+// 					if (changes <= 5)
+// 						break;
+// 				}
+// 				else{
+// 					changes = 6;
+// 				}	
+				
+// 				centroids.erase(centroids.begin(), centroids.end());                      
+// 				update_centroids_median(centroids, pDim, pVec, clusters, tempC, k, d);    		// new centroids
+				
+// 				first = 0;
+// 				count++;
+// 			}
+// 			auto t4 = chrono::high_resolution_clock::now();
+// 			auto durationLloyds = chrono::duration_cast<chrono::microseconds>( t4 - t3 ).count();
+
+// 			for(int i=0; i<k; i++){
+// 				ofile << "CLUSTER-" << i << " {size: " << clusters[i].size() << ", centroid: [";
+// 				for (int y=0; y<d-1; y++){
+// 					ofile << (int)centroids[i][y] << ", ";
+// 				}
+// 				ofile << (int)centroids[i][d-1] << "]}" << endl;
+// 			}
+
+// 			ofile << "clustering_time: " << durationLloyds << endl;
+// 			silhouette(clusters, centroids, pVec, k, d, ofile);
+// 		}
+// 		else{
+// 			cout << "Output file does not exist." << endl;
+// 		}
+// 	}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-	ifstream file (iFile);
-	if (file.is_open()){
-		read_data(file, &magic_number, &number_of_images, &n_rows, &n_cols, pVec, tempVec);
+	ifstream classesFile (classes);
+	if (classesFile.is_open()){
 
 		for(int i = 0; i < k; i++){
-			clusters.push_back(vector<int>());
-			temp.push_back(vector<int>());
+			clusters3.push_back(vector<int>());
 		} 
-		d = n_rows * n_cols;
-		
-		auto t3 = chrono::high_resolution_clock::now();
-		
-		k_means_init(centroids, number_of_images, pVec, k, d);
-		
-		if (ofile.is_open()){															//Lloyd's
-			ofile << endl << "ORIGINAL SPACE" << endl;
 
-			while((count < 40) && (changes > 5)){
-				changes = 0;
-				lloyds_assignment(clusters, temp, number_of_images, pVec, centroids, k, d, &changes, first);
-				
-				if(!first){
-					if (changes <= 5)
-						break;
+		while (!classesFile.eof()){
+	      	getline(classesFile, sline);
+
+			cline = new char[sline.length() + 1];
+			strcpy(cline, sline.c_str());
+
+			pch = strtok(cline," ,{()\n");
+			while (pch != NULL){
+				countw++;
+
+				if (countw > 3){
+					clusters3[countl].push_back(atoi(pch));
 				}
-				else{
-					changes = 6;
-				}	
-				
-				centroids.erase(centroids.begin(), centroids.end());                      
-				update_centroids_median(centroids, pDim, pVec, clusters, tempC, k, d);    		// new centroids
-				
-				first = 0;
-				count++;
+				pch = strtok (NULL, " ,{()\n");
 			}
-			auto t4 = chrono::high_resolution_clock::now();
-			auto durationLloyds = chrono::duration_cast<chrono::microseconds>( t4 - t3 ).count();
-
-			for(int i=0; i<k; i++){
-				ofile << "CLUSTER-" << i << " {size: " << clusters[i].size() << ", centroid: [";
-				for (int y=0; y<d-1; y++){
-					ofile << (int)centroids[i][y] << ", ";
-				}
-				ofile << (int)centroids[i][d-1] << "]}" << endl;
-			}
-
-			ofile << "clustering_time: " << durationLloyds << endl;
-			silhouette(clusters, centroids, pVec, k, d, ofile);
-		}
-		else{
-			cout << "Output file does not exist." << endl;
+			countw = 0;
+			countl++;
 		}
 	}
+
+
+
+
+
+
 	return 0;
 }
